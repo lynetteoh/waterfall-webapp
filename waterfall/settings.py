32a0290b2a156cb05a0a2be6be8df5dfa896d9f6
Panashe Mutema <Panashe@Panashes-MacBook-Pro.local>
2018-08-26 21:08:01 +1000

    Added draft splash page

--- waterfall/settings.py
+++ waterfall/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#print("The base dir is: ", os.path.join(BASE_DIR, "static/"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -113,6 +114,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "waterfall/static/"),
+]
 
 
 # Static files (CSS, JavaScript, Images)
