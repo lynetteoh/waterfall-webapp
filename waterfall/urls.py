f9138f6a4270eb11f11b7eb65bea67b05603a510
stephaniesac <stephanie.a.chua@gmail.com>
2018-09-13 15:34:31 +1000

    dashboard + renamed base templates

--- waterfall/urls.py
+++ waterfall/urls.py
@@ -21,5 +21,6 @@ from webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def index(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
