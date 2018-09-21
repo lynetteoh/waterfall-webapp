f0f7d3b1c10a3af59a68b1c52d91643476c362e2
Joseph Hilsberg <jqhils@Josephs-MacBook-Pro-2.local>
2018-09-21 16:52:45 +1000

    Transfer model added

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from webapp.models import Profile
 from webapp.models import Account, Transaction
-from webapp.models import OneToOnePayment
+from webapp.models import Transfer
 
 admin.site.register(Profile)
 
 admin.site.register(Account)
 admin.site.register(Transaction)
 
-admin.site.register(OneToOnePayment)
+admin.site.register(Transfer)
