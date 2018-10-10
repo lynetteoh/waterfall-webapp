bb71aab7620d309919dca1df9fd70ba3a84bce1e
stephaniesac <stephanie.a.chua@gmail.com>
2018-10-10 21:21:24 +1100

    Tiny fixes

--- webapp/views.py
+++ webapp/views.py
@@ -188,6 +188,8 @@ def pay(request):
                 today = timezone.localize(datetime.today()).date()
                 deadline = \
                     timezone.localize(datetime.strptime(date, "%Y-%m-%d")).date() # yyyy-mm-dd
+                print(deadline)
+                print(today)
                 if deadline < today:
                     raise Exception("Invalid Past Payment Date")
 
