ab7acbca041133216589389f426ec3a3807f4de7
Panashe Mutema <Panashe@Panashes-MacBook-Pro.local>
2018-10-21 20:26:00 +1100

    deploy

--- waterfall/webapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from webapp.models import Profile, Account, Transaction, Transfer, GroupAccount
-
-admin.site.register(Profile)
-admin.site.register(Account)
-admin.site.register(Transaction)
-admin.site.register(Transfer)
-admin.site.register(GroupAccount)
