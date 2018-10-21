a3c215f45c549bf4beb1d92007eaa1e1f0b5aba4
Panashe Mutema <Panashe@Panashes-MacBook-Pro.local>
2018-10-21 19:51:04 +1100

    deployment

--- waterfall/settings.py
+++ waterfall/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  config(‘SECRET_KEY’)
+SECRET_KEY =  config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config(‘DEBUG’, default=False, cast=bool)
@@ -115,6 +115,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, ‘static’),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 
